1f6c31eb0c02cc48b80cb2adbedaac5e36c038d0
Katsuyuki-Karasawa <4ranci0ne@gmail.com>
2024-03-18 21:25:08 +0900

    fix

--- vite-bun-plugin.ts
+++ vite-bun-plugin.ts
@@ -1,60 +1,63 @@
-import { builtinModules } from 'module'
-import type { Plugin, UserConfig } from 'vite'
+import { builtinModules } from 'node:module'
 
-export const bunServerPlugin = (): Plugin => {
-  const virtualEntryId = 'virtual:node-server-entry-module'
+export const bunServerPlugin = () => {
+  const virtualEntryId = 'virtual:bun-server-entry-module'
   const resolvedVirtualEntryId = '\0' + virtualEntryId
 
   return {
-    name: '@hono/vite-node-server',
-    resolveId(id) {
+    name: '@hono/bun-server',
+    resolveId(id: string) {
       if (id === virtualEntryId) {
         return resolvedVirtualEntryId
       }
     },
-    async load(id) {
+    load(id: string) {
       if (id === resolvedVirtualEntryId) {
         return `import { Hono } from 'hono'
-        import { serveStatic } from '@hono/node-server/serve-static'
-        import { serve } from '@hono/node-server'
-        
-        const worker = new Hono()
-        worker.use('/static/*',   serveStatic({root: './dist'}))
-        
-        const modules = import.meta.glob(['/app/server.ts'], { import: 'default', eager: true })
-        for (const [, app] of Object.entries(modules)) {
-          if (app) {
-            worker.route('/', app)
-            worker.notFound(app.notFoundHandler)
-          }
-        }
-        
-        serve({ ...worker, port: 3000 }, info => {
-          console.log('Listening on http://localhost:'+info.port)
-        })`
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const worker = new Hono()
+worker.use('/static/*', serveStatic({ root: './dist' }))
+
+const modules = glob('/app/server.ts', { import: 'default', eager: true })
+for (const [, app] of Object.entries(modules)) {
+  if (app) {
+    worker.route('/', app)
+    worker.notFound(app.notFoundHandler)
+  }
+}
+
+Bun.serve({
+  fetch: worker.fetchHandler,
+  port: process.env.PORT || 3000,
+  development: process.env.NODE_ENV !== 'production',
+  logger: {
+    info(req, { statusCode, startTime }) {
+      const duration = Date.now() - startTime
+      console.log(
+        \`\${req.method} \${req.url} \${statusCode} \${duration}ms\`
+      )
+    },
+  },
+})
+`
       }
     },
-    config: async (): Promise<UserConfig> => {
-      return {
-        ssr: {
-          external: [],
-          noExternal: true,
-        },
-        build: {
-          outDir: './dist',
-          emptyOutDir: false,
-          minify: true,
-          ssr: true,
-          rollupOptions: {
-            external: [...builtinModules, /^node:/],
-            input: virtualEntryId,
-            output: {
-              entryFileNames: 'server.mjs',
-            },
+    config: () => ({
+      build: {
+        outDir: './dist',
+        emptyOutDir: false,
+        minify: process.env.NODE_ENV === 'production',
+        ssr: true,
+        rollupOptions: {
+          external: [...builtinModules, /^node:/],
+          input: virtualEntryId,
+          output: {
+            entryFileNames: 'server.mjs',
           },
         },
-      }
-    },
+      },
+    }),
   }
 }
 
